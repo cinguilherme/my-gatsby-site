183c6c740ba062e1700b79a9f9445070e444e70c
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-06-13 16:08:27 -0300

    minor changes, added my pic, removed some of the default text

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## this is my site using gatsby as a study project. Also this will be my go to platform to test new apis
 
 ### this was originally created using the Gatsby CLI new default project.
+
+#### The main goal of this site is just to be a platform to consume most of my other projects, several restfull apis.. wasm stuff.. I realise Gatsby is meant to static stuff but I feel like it is a joy to buuild like several of small SPAs. Like I can have a single page(feature for any of the off board stuff) This sounds interesting to be that just want that.
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.css';
 
 import Header from "./header"
 import "./layout.css"
@@ -26,6 +27,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+
       <div
         style={{
           margin: `0 auto`,
@@ -33,13 +35,23 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
+
         <main>{children}</main>
-        <footer>
+
+
+
+
+        <footer style={{ marginTop: '2em' }}>
           © {new Date().getFullYear()}, Built with
-          {` `}
+              {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
+
+
+
       </div>
+
+
     </>
   )
 }
