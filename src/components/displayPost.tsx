163c6e0b5a9e4b9d5dba99cf7a7e039d028d24cd
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-06-14 13:26:25 -0300

    added simplest list of posts on main page with hooks

--- src/components/displayPost.tsx
+++ src/components/displayPost.tsx
@@ -1,1 +1,44 @@
 import React from "react"
+
+import { useStaticQuery, graphql } from "gatsby"
+
+function DisplayPosts() {
+  const posts = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            html
+            excerpt
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
+
+  const files = posts.allMarkdownRemark.edges
+
+  const buildOl = () => {
+    return (
+      <ol>
+        {files.map(el => (
+          <li key={el.node.frontmatter.title}>{el.node.excerpt}</li>
+        ))}
+      </ol>
+    )
+  }
+
+  return (
+    <div>
+      <p>posts to be displayed</p>
+      {buildOl()}
+    </div>
+  )
+}
+
+export default DisplayPosts
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,31 +3,36 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import DisplayPosts from "../components/displayPost"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage = () => {
 
-    <div className="row">
+  return (
+    <Layout>
+      <SEO title="Home" />
 
-      <div className="col-md-8">
-        <h1>Hi Folks</h1>
-        <h3>presenting myself, I am a developer both professionally and casually</h3>
-        <p>Welcome to my Gatsby site, this is meant to be my playground for several things I want to implement/try on a app build with react and Gatsby.</p>
+      <div className="row">
+
+        <div className="col-md-8">
+          <h1>Hi Folks</h1>
+          <h3>presenting myself, I am a developer both professionally and casually</h3>
+          <p>Welcome to my Gatsby site, this is meant to be my playground for several things I want to implement/try on a app build with react and Gatsby.</p>
+        </div>
+
+        <div className="col-md-4" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+
       </div>
 
-      <div className="col-md-4" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
+      <div>
+        list of posts will be here.. paginated and stuff
+        <DisplayPosts />
       </div>
+    </Layout>
+  )
 
-    </div>
+}
 
-
-    <div>
-      list of posts will be here.. paginated and stuff
-    </div>
-
-  </Layout>
-)
 
 export default IndexPage
